dda6520f0a2d961cd9efb02743a490c97c954bf6
OdagakiHiroki <email@example.com>
2021-02-14 22:41:45 +0900

    add input component

---
+++ pages/ContentsRegister.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="contents-register">
+    <BaseInput
+      class="base-input name"
+      :value="name"
+      @changeValue="changeInputName"
+    />
+    <div>入力値は: {{name}}</div>
+    <BaseInput
+      class="base-input birth-day"
+      :value="birthDay"
+      :disabled="birthDayDisabled"
+      @changeValue="changeInputBirthDay"
+    />
+    <div>入力値は: {{birthDay}}</div>
+    <BaseInput
+      class="base-input address"
+      :value="address"
+      :disabled="addressDisabled"
+      @changeValue="changeInputAddress"
+    />
+    <div>入力値は: {{address}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentsRegister',
+
+  data() {
+    return {
+      name: null,
+      birthDay: null,
+      address: null,
+    };
+  },
+
+  computed: {
+    birthDayDisabled() {
+      return this.name === null;
+    },
+    addressDisabled() {
+      return this.birthDay === null;
+    },
+  },
+
+  methods: {
+    changeInputName(value) {
+      this.name = value;
+    },
+    changeInputBirthDay(value) {
+      this.birthDay = value;
+    },
+    changeInputAddress(value) {
+      this.address = value;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.contents-register{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  .base-input{
+    width: 100px;
+    & + .base-input {
+      margin-top: 10px;
+    }
+  }
+}
+</style>
